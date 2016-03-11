928062ecc178893f0169cc8b4278f919d788fe64
SunilGC <sunilgc0@gmail.com>
2016-03-11 02:54:31 -0700

    beta1

--- script.js
+++ script.js
@@ -18,7 +18,28 @@ myImage.onclick =  function() {
 	clearInterval(intervalHandle);
 };
 
+var socialMedia = {
+  facebook : 'http://facebook.com/sunilgc89',
+  twitter: 'http://twitter.com/sunilgc',
+  linkedin: 'https://www.linkedin.com/in/sunilgc',
+  instagram: 'https://www.instagram.com/sunilfilter/'
+};
 
+var socialList = function() {
+  var  output = '<ul>', 
+    myList = document.querySelectorAll('.socialmediaicons');
+
+  for (var key in arguments[0]) {
+    output+= '<li><a href="' + socialMedia[key] + '">' +
+      '<img src="images/' + key + '.png" alt="icon for '+key+'">' +
+      '</a></li>';
+  }
+  output+= '</ul>';
+  
+  for (var i = myList.length - 1; i >= 0; i--) {
+    myList[i].innerHTML = output;
+  };
+}(socialMedia);
 
 
 
